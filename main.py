a044abc38b47d526d3ef74df7117bbffa7a0bbb9
Sung Ahn <superahn@gmail.com>
2022-10-05 04:11:13 +0000

    Add file.py to save the result

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from extractors.woori import extract_woori_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
+from extractors.file import save_to_file
 
 # print("WOORI JOBS:  ")
 # woori_jobs = extract_woori_jobs("")
@@ -12,7 +13,4 @@ indeed_jobs = extract_indeed_jobs(keyword)
 wwr_jobs = extract_wwr_jobs(keyword)
 jobs = indeed_jobs + wwr_jobs
 
-print(f"Total: {len(indeed_jobs)+len(wwr_jobs)}, indeed:{len(indeed_jobs)}, wwr:{len(wwr_jobs)}")
-for job in jobs:
-  print(job)
-  print("///////////////")
+save_to_file(keyword, jobs)
